659819efb01a8dcc8a5f79f04c16e4b436216790
Git-junior <juni0r.kiza6@gmail.com>
2022-01-18 18:24:36 +0900

    createアクションのコントローラー定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,12 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :category_id, :situation_id, :delivery_charge_id, :prefecture_id, :delivery_time_id, :price).merge(user_id: current_user.id)
+  end
 end

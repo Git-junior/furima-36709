55b6f5ed59cfcd03e3f1790e597e344ea599a14d
Git-junior <juni0r.kiza6@gmail.com>
2022-01-19 21:20:21 +0900

    ActiveHashのアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
+  
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :delivery_time
   validates :image, presence: true
   validates :name, presence: true
   validates :text, presence: true

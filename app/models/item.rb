b73a27e8b7a058e46526289f4037640a6fa045bb
Git-junior <juni0r.kiza6@gmail.com>
2022-01-19 18:30:05 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :category_id, presence: true
+  validates :situation_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_time_id, presence: true
+  validates :price, presence: true, format: { with: /\A[\d]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end

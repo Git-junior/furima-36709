d5b0e54576fc04e6c20ea6452b39d73e82586ea3
Git-junior <juni0r.kiza6@gmail.com>
2022-01-21 10:05:26 +0900

    モデルの単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  
+
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -11,10 +11,10 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true
   validates :text, presence: true
-  validates :category_id, presence: true
-  validates :situation_id, presence: true
-  validates :delivery_charge_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :delivery_time_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 }
+  validates :situation_id, presence: true, numericality: { other_than: 1 }
+  validates :delivery_charge_id, presence: true, numericality: { other_than: 1 }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
+  validates :delivery_time_id, presence: true, numericality: { other_than: 1 }
   validates :price, presence: true, format: { with: /\A[\d]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name               {'サイコロ'}
+    text               {'JPEGサイコロ'}
+    category_id        {'2'}
+    situation_id       {'2'}
+    delivery_charge_id {'2'}
+    prefecture_id      {'2'}
+    delivery_time_id   {'2'}
+    price              {'1000'}
+    association        :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/sample.jpeg'), filename: 'sample.jpeg')
+    end
   end
 end

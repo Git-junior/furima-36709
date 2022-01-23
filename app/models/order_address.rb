6b86caa12d0053add4ce1dc1bb75c9b9b1e887bf
Git-junior <juni0r.kiza6@gmail.com>
2022-01-23 15:40:05 +0900

    バリデーションの定義

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,2 +1,19 @@
 class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :prefecture_id,  numericality: { other_than: 1 }
+    validates :municipalities
+    validates :address
+    validates :telephone_number, format: {with: /\A[0-9]{10, 11}\z/}
+  end
+
+  def save
+    order = Order.create(user_id; user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, address: address, building_name: building_name, telephone_number: telephone_number, order_id: order.id)
+  end
 end

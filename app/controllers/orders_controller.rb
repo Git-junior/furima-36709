00e56fb9c7b144c6923c5e6516c76987de639188
Git-junior <juni0r.kiza6@gmail.com>
2022-01-26 16:35:12 +0900

    （追加）ordersコントローラーの編集（トップページに遷移）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,7 +25,7 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    if current_user == @item.user
+    if current_user == @item.user || @item.order
       redirect_to root_path
     end
   end

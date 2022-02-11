020422672ebc3392b2acd71d99ba538b56b02955
Git-junior <juni0r.kiza6@gmail.com>
2022-02-11 21:36:47 +0900

    メッセージの文末の統一

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,11 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true
   validates :text, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1, message: 'を選択して下さい' }
-  validates :situation_id, presence: true, numericality: { other_than: 1, message: 'を選択して下さい' }
-  validates :delivery_charge_id, presence: true, numericality: { other_than: 1, message: 'を選択して下さい' }
-  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: 'を選択して下さい' }
-  validates :delivery_time_id, presence: true, numericality: { other_than: 1, message: 'を選択して下さい' }
+  validates :category_id, presence: true, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :situation_id, presence: true, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :delivery_charge_id, presence: true, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :delivery_time_id, presence: true, numericality: { other_than: 1, message: 'を選択してください' }
   validates :price, presence: true, format: { with: /\A[\d]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   has_one :order
   has_many :comments

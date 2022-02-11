c0065206047cdb52b8f97789feccc5b3c802abef
Git-junior <juni0r.kiza6@gmail.com>
2022-02-11 18:53:30 +0900

    日本語化用のファイル（Itemモデル関連）の作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,11 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true
   validates :text, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1 }
-  validates :situation_id, presence: true, numericality: { other_than: 1 }
-  validates :delivery_charge_id, presence: true, numericality: { other_than: 1 }
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
-  validates :delivery_time_id, presence: true, numericality: { other_than: 1 }
+  validates :category_id, presence: true, numericality: { other_than: 1, message: 'を選択して下さい' }
+  validates :situation_id, presence: true, numericality: { other_than: 1, message: 'を選択して下さい' }
+  validates :delivery_charge_id, presence: true, numericality: { other_than: 1, message: 'を選択して下さい' }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: 'を選択して下さい' }
+  validates :delivery_time_id, presence: true, numericality: { other_than: 1, message: 'を選択して下さい' }
   validates :price, presence: true, format: { with: /\A[\d]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   has_one :order
   has_many :comments
